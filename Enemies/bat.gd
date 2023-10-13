ef14d87cdfbaf9e0e180a699490c686a92540b8e
zakwindy <z.a.windham@gmail.com>
2023-10-13 13:57:30 -0400

    P14 progress

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 const FRICTION = 200
 const KNOCKBACK = 120
 
+@onready var stats = $Stats
+
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide() 
 
 
 func _on_hurtbox_area_entered(area):
+	stats.health -= area.damage
 	velocity = area.knockback_vector * KNOCKBACK
+
+
+func _on_stats_no_health():
+	queue_free()
